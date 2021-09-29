af4e8412241db48c4828c363b716a026bdb4fe5e
Diego López Torres <buyeye09@gmail.com>
2021-09-29 00:27:07 -0500

    App.js: Added a modal that pops when the user visits the app for first time.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
+import Swal from 'sweetalert2';
+
 import Header from './components/Header';
 import Form from './components/Form/Form';
 
-const App = () => (
-    <Fragment>
-        <Header/>
-        <Form/>
-    </Fragment>
-);
+const App = () => {
+
+    const html = '<strong>Encuentra mi Estación</strong> es una aplicación que le ayudará a llegar a su destino mediante' +
+        'el tren ligero, solo ingrese la estación donde se encuentre o vaya a ir, la estación a la que quiera llegar y' +
+        'nosotros le diremos cual es la mejor forma de ayudar';
+
+    useEffect(() => {
+        const welcomeModal = async () => {
+            const hasVisitedBefore = localStorage.getItem('visited');
+            if(!hasVisitedBefore) {
+                await Swal.fire({
+                    title: '¡Bienvenido a Encuentra mi Estación!',
+                    html,
+                    confirmButtonText: 'Entendido'
+                });
+                localStorage.setItem('visited', 'true');
+            }
+        }
+        welcomeModal();
+    }, []);
+
+    return (
+        <Fragment>
+            <Header/>
+            <Form/>
+        </Fragment>
+    );
+};
 
 export default App;
